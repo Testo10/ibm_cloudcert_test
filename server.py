7bf1dde58fd987dc7f950f6afc69c2670df1348f
Testo10 <ibrahimlearning10@gmail.com>
2023-07-15 01:55:40 +0300

    Initial commit

--- server.py
+++ server.py
@@ -1,17 +1,32 @@
-import machinetranslation
-from flask import Flask
+from machinetranslation import translator, testes
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/translate/english-to-french/<text>')
-def translate_english_to_french(text):
-    translated_text = machinetranslation.english_to_french(text)
-    return translated_text
 
-@app.route('/translate/french-to-english/<text>')
-def translate_french_to_english(text):
-    translated_text = machinetranslation.french_to_english(text)
-    return translated_text
+@app.route('/', methods=['GET', 'POST'])
+def translate():
+    text = ''
+    translation = None
 
-if __name__ == '__main__':
-    app.run()
+    if request.method == 'POST':
+        text = request.form['text']
+
+        if request.form['translate'] == 'French':
+            translation = translator.english_to_french(text)
+        elif request.form['translate'] == 'English':
+            translation = translator.french_to_english(text)
+
+    return render_template('index.html', text=text, translation=translation)
+
+
+if __name__ == "__main__":
+    app.run(port=4926)
+
+#@app.route('/translate/french-to-english/<text>')
+#def translate_french_to_english(text):
+ #   translated_text = machinetranslation.french_to_english(text)
+  #  return translated_text
+
+#if __name__ == '__main__':
+ #   app.run()
